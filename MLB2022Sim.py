090ca8740cc64cebc65a828cc3569dcff4a978b6
Stephen42Kim <stephen42kim@gmail.com>
2023-04-22 17:15:05 -0500

    Update MLB2022Sim.py

--- MLB2022Sim.py
+++ MLB2022Sim.py
@@ -8,9 +8,11 @@ Created on Mon Mar 20 04:17:37 2023
 import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup
-import TournamentSimulator
+import requests
+import re
+import TournamentSimulator as ts
 
-class MLB2022Simulator(TournamentSimulator):
+class MLB2022Simulator(ts.TournamentSimulator):
     '''Class to simulate MLB 2022 Playoff scenario'''
     
     def __init__(self):
@@ -33,7 +35,45 @@ def BuildDataset(year):
     '''Function that builds dictionaries for the national and american league playoff
     teams. Extract data from baseball-reference.com'''
     
-    with open('https://www.baseball-reference.com/leagues/majors/2022-team-pitching-staffs.shtml') as fp:
-        soup = BeautifulSoup(fp, 'html.parser')
-        
-    print(soup)
+    driver.get('https://www.baseball-reference.com/leagues/majors/2022-team-pitching-staffs.shtml')
+    time.sleep(5)
+    driver.execute_script("window.scrollTo(0, 1500);")
+    
+BuildDataset(2022)
+
+
+
+## Pulls a single table from a url provided by the user.
+## The desired table should be specified by tableID.
+## This function is used in all functions that do more complicated pulls.
+def pullTable(url, tableID):
+    res = requests.get(url)
+    ## Work around comments
+    comm = re.compile("<!--|-->")
+    soup = BeautifulSoup(comm.sub("", res.text), 'lxml')
+    tables = soup.findAll('table', id = tableID)
+    data_rows = tables[0].findAll('tr')
+    data_header = tables[0].findAll('thead')
+    data_header = data_header[0].findAll("tr")
+    data_header = data_header[0].findAll("th")
+    game_data = [[td.getText() for td in data_rows[i].findAll(['th','td'])]
+        for i in range(len(data_rows))
+        ]
+    data = pd.DataFrame(game_data)
+    header = []
+    for i in range(len(data.columns)):
+        header.append(data_header[i].getText())
+    data.columns = header
+    data = data.loc[data[header[0]] != header[0]]
+    data = data.reset_index(drop = True)
+    return(data)
+## For example:
+## url = "http://www.baseball-reference.com/teams/KCR/2016.shtml"
+## pullTable(url, "team_batting")
+
+url = "https://www.baseball-reference.com/leagues/majors/2022-team-pitching-staffs.shtml"
+pullTable(url, "pitchers")
+
+
+url = "http://www.baseball-reference.com/teams/KCR/2016.shtml"
+pullTable(url, "team_batting")
